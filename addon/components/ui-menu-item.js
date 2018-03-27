b81972cd692341f236ed84f209ebc4af5e8c9c83
Edward Huang <ehuang@soxhub.com>
2018-03-27 14:59:26 +0800

    updated to menu-list

--- addon/components/ui-menu-item.js
+++ addon/components/ui-menu-item.js
@@ -7,10 +7,10 @@ export default Component.extend(Styled, {
   tagName: 'li',
   layout,
   styles: {
-    base: 'db hover-bg-light-gray no-underline link p-t-6 p-b-6 dark-gray',
+    base: 'db hover-bg-light-gray no-underline link p-t-6 p-b-6 dark-gray mb0 cursor-pointer',
     defaultStyle: 'small',
 
-    colors: group({
+    sizes: group({
       small: 'p-l-15 p-r-15',
       medium: 'p-l-20 p-r-20',
       large: 'p-l-35 p-r-35'
--- app/components/ui-menu-dropdown.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'soxhub-ui-components/components/ui-menu-dropdown';
