19a39b8df6070e636a4c99a0bff4d4cd05e42fac
Edward Huang <ehuang@soxhub.com>
2018-03-27 15:43:08 +0800

    updated styling

--- tests/dummy/app/components/demo-menu-list.js
+++ tests/dummy/app/components/demo-menu-list.js
@@ -2,5 +2,16 @@ import Component from '@ember/component';
 import layout from '../templates/components/demo-menu-list';
 
 export default Component.extend({
-  layout
+  layout,
+  selectedItem: 'All Comments',
+  menuItems: [
+    'All Comments',
+    'Controls',
+    'Tests',
+    'Narratives',
+    'Issues',
+    'Workstream',
+    'OpsAudits',
+    'Closed Comments'
+  ]
 });
