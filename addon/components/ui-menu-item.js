4edcd08f16e0918a2f86778dd0defe1c6fe9a20c
Edward Huang <ehuang@soxhub.com>
2018-03-30 20:04:14 +0800

    update

--- addon/components/ui-menu-item.js
+++ addon/components/ui-menu-item.js
@@ -5,17 +5,14 @@ import { Styled, group } from '../lib/ui-components';
 export default Component.extend(Styled, {
   tagName: 'li',
   layout,
-  init() {
-    this._super(...arguments);
-    this.set('styles', {
-      base: 'db hover-bg-light-gray no-underline link p-t-6 p-b-6 dark-gray mb0 cursor-pointer',
-      defaultStyle: 'small',
+  styles: {
+    base: 'db hover-bg-light-gray no-underline link p-t-6 p-b-6 dark-gray mb0 cursor-pointer',
+    defaultStyle: 'small',
 
-      sizes: group({
-        small: 'p-l-15 p-r-15',
-        medium: 'p-l-20 p-r-20',
-        large: 'p-l-35 p-r-35'
-      })
-    });
+    sizes: group({
+      small: 'p-l-15 p-r-15',
+      medium: 'p-l-20 p-r-20',
+      large: 'p-l-35 p-r-35'
+    })
   }
 });
